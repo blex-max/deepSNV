0fe8d9351ee58c34105669a1e1094aa3cb964d7c
blex-max <ablex7@gmail.com>
2025-08-27 16:09:28 +0100

    fix testing var usage

--- src/tests/test_bam2r-pileup.cpp
+++ src/tests/test_bam2r-pileup.cpp
@@ -12,13 +12,13 @@ const NTParams p1 {
 };
 // allocate counts via vector, but use pointer for the function via .data()
 // 1 pos, 2 strands of data, 11 fields per strand - 1 row of a 3D arr, so 2D arr
-std::vector<int> counts(N_COUNTS_FIELD*2, 0);
+// std::vector<int> counts(N_COUNTS_FIELD*2, 0);
 
 
 // NOTE: also test head_clip, and different base call
 TEST_CASE("score_pile() first member good", "[Overlap Behaviour Test]") {
   khash_t(strh)* kh = kh_init(strh);
-  auto test_counts = counts;
+  std::vector<int> test_counts(N_COUNTS_FIELD*2, 0);
 
   // get qname into overlap table, data into counts table
   static constexpr PileupRead r1{
@@ -44,7 +44,7 @@ TEST_CASE("score_pile() first member good", "[Overlap Behaviour Test]") {
   // counts shouldn't change as it should be rejected for having the same qname
   auto r2 = r1;
   r2.base_q = 9;  // below threshold
-  score_pile(r2, counts.data(), p1, kh);
+  score_pile(r2, test_counts.data(), p1, kh);
   REQUIRE( test_counts == test_snapshot );
 
   kh_destroy(strh, kh);
@@ -53,7 +53,7 @@ TEST_CASE("score_pile() first member good", "[Overlap Behaviour Test]") {
 
 TEST_CASE("score_pile() first member bad qual", "[Overlap Behaviour Test]") {
   khash_t(strh)* kh = kh_init(strh);
-  auto test_counts = counts;
+  std::vector<int> test_counts(N_COUNTS_FIELD*2, 0);
 
   // get qname into overlap table, data into counts table
   static constexpr PileupRead r1{
@@ -79,7 +79,7 @@ TEST_CASE("score_pile() first member bad qual", "[Overlap Behaviour Test]") {
   // for better 2nd member of read pair
   auto r2 = r1;
   r2.base_q = 11;  // below threshold
-  score_pile(r2, counts.data(), p1, kh);
+  score_pile(r2, test_counts.data(), p1, kh);
   REQUIRE( test_counts != test_snapshot );  // counts should not match snapshot (fails, indicating BUG)
 
   kh_destroy(strh, kh);
@@ -88,7 +88,7 @@ TEST_CASE("score_pile() first member bad qual", "[Overlap Behaviour Test]") {
 
 TEST_CASE("score_pile() first member bad pos (head clip)", "[Overlap Behaviour Test]") {
   khash_t(strh)* kh = kh_init(strh);
-  auto test_counts = counts;
+  std::vector<int> test_counts(N_COUNTS_FIELD*2, 0);
 
   // get qname into overlap table, data into counts table
   static constexpr PileupRead r1{
@@ -105,7 +105,7 @@ TEST_CASE("score_pile() first member bad pos (head clip)", "[Overlap Behaviour T
   REQUIRE( ret == 0 );  // check success
   CAPTURE( test_counts );
   REQUIRE( test_counts[5] == 1 );  // base recorded as N in counts table
-  REQUIRE( test_counts[10] == 10 );  // mapq correct
+  REQUIRE( test_counts[10] == 0 );  // mapq correct (0 as not recorded if head_clip)
 
   auto test_snapshot = test_counts;
   assert(test_counts == test_snapshot);  // sanity
@@ -114,7 +114,7 @@ TEST_CASE("score_pile() first member bad pos (head clip)", "[Overlap Behaviour T
   // for better 2nd member of read pair
   auto r2 = r1;
   r2.qpos = 6;  // above head clip threshold
-  score_pile(r2, counts.data(), p1, kh);
+  score_pile(r2, test_counts.data(), p1, kh);
   REQUIRE( test_counts != test_snapshot );  // counts should not match snapshot (fails, indicating BUG)
 
   kh_destroy(strh, kh);
