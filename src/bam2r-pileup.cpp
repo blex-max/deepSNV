fb92aa20e6d49e16661a37e075fcf839622e897d
blex-max <ablex7@gmail.com>
2025-10-21 10:22:54 +0100

    add extra throw hail mary

--- src/bam2r-pileup.cpp
+++ src/bam2r-pileup.cpp
@@ -214,6 +214,7 @@ int bam2R_pileup_function (const bam_pileup1_t *pileups_ptr,
             collate_alleles (nttable.params, pinfo, collated_pileup);
         } catch (std::exception &e) {
             kh_destroy (strh, collated_pileup);
+            throw e;
             return 1; // fail
         }
     }
